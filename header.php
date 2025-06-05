bc7102cd35221994fdad4888564c772d4e2067b3
Robin Winters <robinmelsaw@gmail.com>
2025-06-05 02:58:07 +0100

    refactor(header): move layout to template-parts for cleaner structure

--- header.php
+++ header.php
@@ -8,40 +8,5 @@
   <link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500;600;700&display=swap" rel="stylesheet">
 </head>
 <body>
-  <header class="site-header" role="banner">
-    <div class="header-container">
 
-
-      <div class="logo-left">
-        <img src="<?= get_template_directory_uri(); ?>/assets/images/invisalign.png" alt="Invisalign Diamond Provider" />
-      </div>
-
- 
-      <div class="logo-separator" role="presentation" aria-hidden="true"></div>
-
-
-      <div class="logo-center">
-        <img src="<?= get_template_directory_uri(); ?>/assets/images/dental-suite-logo.png" alt="The York Dental Suite" />
-      </div>
-
-
-      <div class="header-right">
-        <a href="#contact" class="book-button">BOOK NOW</a>
-        <div class="contact-info">
-          <div>
-            <img src="<?= get_template_directory_uri(); ?>/assets/images/phone-icon.svg" alt="" class="contact-icon" aria-hidden="true" />
-            <span class="icon-separator" role="presentation" aria-hidden="true"></span>
-            <a href="tel:01904937041">01904 937 041</a>
-          </div>
-          <div>
-            <img src="<?= get_template_directory_uri(); ?>/assets/images/email-icon.svg" alt="" class="contact-icon" aria-hidden="true" />
-            <span class="icon-separator" role="presentation" aria-hidden="true"></span>
-            <a href="mailto:info@theyorkdentalsuite.co.uk">info@theyorkdentalsuite.co.uk</a>
-          </div>
-        </div>
-      </div>
-
-    </div>
-  </header>
-</body>
-</html>
+<?php get_template_part('template-parts/header'); ?>
